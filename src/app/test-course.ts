659d338435a66a345470e10482aa60ce219f3981
Aayesha-Ali <aayeshali123@gmail.com>
2022-06-12 18:11:01 +1200

    Initial commit

---
+++ src/app/test-course.ts
@@ -0,0 +1,41 @@
+import { Course } from './course';
+
+export const COURSES: Course[] = [
+  // ----insert array of five courses with ID here-----
+
+  {
+    course_id: 1,
+    course_title: 'Web Development',
+    semester: 'One',
+    period: 'Wednesday 4-6 PM',
+    lecturer: 'Dr Jian Yu',
+  },
+  {
+    course_id: 2,
+    course_title: 'Research and Development',
+    semester: 'One',
+    period: 'Tuesday 2-4 PM',
+    lecturer: 'Ramesh Lal',
+  },
+  {
+    course_id: 3,
+    course_title: 'Programming 2',
+    semester: 'One',
+    period: 'Tuesday 9-10 AM',
+    lecturer: 'Kenneth Johnson',
+  },
+  {
+    course_id: 4,
+    course_title: 'Applied Human Interaction',
+    semester: 'Two',
+    period: 'Tuesday 8-10 AM',
+    lecturer: 'Robert Wellington',
+  },
+  {
+    course_id: 5,
+    course_title: 'Comp Network Applications',
+    semester: 'Two',
+    period: 'Monday 2-4 PM',
+    lecturer: 'Michael Hadrup',
+  },
+];
